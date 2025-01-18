d395f6f789e9cba3ee8f14ddd6b45c7fdf2c8e16
Sameer Watts <sameerwatts00@gmail.com>
2025-01-18 19:07:03 +0530

    Setup initial project

---
+++ next-js-study/app/awesome/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AwesomePage = () => {
+    return (
+        <main>
+            <h1>Next js Is Awesome</h1>
+        </main>
+    );
+};
+
+export default AwesomePage;
