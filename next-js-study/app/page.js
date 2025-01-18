359a689bc30993c0112189e725606e2faa2d68d2
Sameer Watts <sameerwatts00@gmail.com>
2025-01-18 21:11:32 +0530

    Foodies project initial setup

--- next-js-study/app/page.js
+++ next-js-study/app/page.js
@@ -1,13 +1,9 @@
-import Link from "next/link";
-import Header from "../component/header";
-
 export default function Home() {
   return (
     <main>
-      <Header/>
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p><Link href="/about">About Us</Link></p>
-      <p><Link href="/blog">Blog</Link></p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
