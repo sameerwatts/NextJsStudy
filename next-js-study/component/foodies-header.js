f7566268adb1ac380966a295421d680c6a58e0f7
Sameer Watts <sameerwatts00@gmail.com>
2025-01-20 11:49:15 +0530

    Use Image component

--- next-js-study/component/foodies-header.js
+++ next-js-study/component/foodies-header.js
@@ -2,12 +2,13 @@ import Link from "next/link";
 import React from "react";
 import logoImg from "@/assets/logo.png";
 import classes from './foodies-header.module.css';
+import Image from "next/image";
 
 const FoodiesHeader = () => {
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
-        <img src={logoImg.src} atl="A Plate with Food on it" />
+        <Image src={logoImg} alt="A Plate with Food on it" priority />
         Next Level Food
       </Link>
       <nav className={classes.nav}>
