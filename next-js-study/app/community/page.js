de566f819218b42b7dc9837b988875cd2d807794
Sameer Watts <sameerwatts00@gmail.com>
2025-01-20 09:49:20 +0530

    Exercies solution

---
+++ next-js-study/app/community/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Community = () => {
+    return (
+        <h1>
+         Community page   
+        </h1>
+    );
+};
+
+export default Community;
---
+++ next-js-study/app/meals/share/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Share = () => {
+    return (
+        <h1>
+            Share page
+        </h1>
+    );
+};
+
+export default Share;
